cd1fd82f4805e39c3bf6f59785433d6c8665aa3c
P.SANTHOSH <santhoshpepakayala9951@gmail.com>
2026-04-27 15:46:08 +0530

    add print('Invalid username or password')

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -69,6 +69,7 @@ def login():
 
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            print('Invalid username or password')
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
